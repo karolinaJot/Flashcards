6ee5070ea9960d774f427fb83600fc7f59065dd9
karolinaJot <amicus.karo@gmail.com>
2021-01-31 23:21:58 +0100

    Add basic validation

--- Flashcards.Web/Models/JSFlashcardsModel.cs
+++ Flashcards.Web/Models/JSFlashcardsModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace Flashcards.Web.Models
 	public class JSFlashcardsModel
 	{
 		public int Id { get; set; }
+
+		[Required]
 		public string Title { get; set; }
+		[Required]
 		public string Description { get; set; }
 
 	}
