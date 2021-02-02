a5bb7b0116df187c745f3b11c52e2d54060aeb03
karolinaJot <amicus.karo@gmail.com>
2021-02-03 00:17:29 +0100

    working on JS file

--- Flashcards.Web/wwwroot/js/flashcardsCarousel.js
+++ Flashcards.Web/wwwroot/js/flashcardsCarousel.js
@@ -1,1 +1,23 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+	console.log("DOM ready to use!");
+
+	let dataList = Array.from(document.querySelectorAll("#flashcardsList li"));
+	console.log(dataList);
+	const flashcardsElementsList = [];
+
+	function retriveData(list) {
+		const flashcardsObjList = [];
+		for (let i = 0; i++; i < list.length) {
+			let temp = Array.from(list[i]);
+			console.log(temp);
+			flashcardsElementsList.push(temp);
+			console.log(flashcardsElementsList);
+
+		}
+		return flashcardsElementsList;
+	}
+
+	retriveData(dataList);
+
+
+});
