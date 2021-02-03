88523b25d5aaf2cff34de852b1963d1b39f587db
karolinaJot <amicus.karo@gmail.com>
2021-02-03 14:38:12 +0100

    Add API Controller, convert flashcards data into JSON file and send to DisplayJS page

---
+++ Flashcards.Web/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Flashcards.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Flashcards.Web.Controllers
+{
+	[ApiController]
+	[Route("api/[action]")]
+	public class ApiController : Controller
+	{
+		private readonly ApplicationDbContext _dbContext;
+		public ApiController(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			return Json(new { data = _dbContext.FlashcardsJS.ToList() });
+		}
+	}
+}
--- Flashcards.Web/wwwroot/js/flashcardsCarousel.js
+++ Flashcards.Web/wwwroot/js/flashcardsCarousel.js
@@ -1,23 +1,18 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+﻿
+document.addEventListener("DOMContentLoaded", function () {
 	console.log("DOM ready to use!");
 
-	let dataList = Array.from(document.querySelectorAll("#flashcardsList li"));
-	console.log(dataList);
-	const flashcardsElementsList = [];
+	const endpoint = "/api/getAll";
 
-	function retriveData(list) {
-		const flashcardsObjList = [];
-		for (let i = 0; i++; i < list.length) {
-			let temp = Array.from(list[i]);
-			console.log(temp);
-			flashcardsElementsList.push(temp);
-			console.log(flashcardsElementsList);
-
-		}
-		return flashcardsElementsList;
+	async function getAll() {
+		const response = await fetch(endpoint);
+		const flashcardsData = await response.json();
+		console.log(flashcardsData.data);
 	}
 
-	retriveData(dataList);
+	getAll();
+
+
 
 
 });
