35e00d641a34947d7af40fc7e71012da39a7401b
karolinaJot <amicus.karo@gmail.com>
2021-02-04 23:17:34 +0100

    TO FIX: carousel navigation

--- Flashcards.Web/wwwroot/js/flashcardsCarousel.js
+++ Flashcards.Web/wwwroot/js/flashcardsCarousel.js
@@ -5,13 +5,43 @@ document.addEventListener("DOMContentLoaded", function () {
 	const endpoint = "/api/getAll";
 
 	async function getAll() {
+
+		//dodać walidację jeśli nie będzie żadnej fiszki
 		const response = await fetch(endpoint);
-		const flashcardsData = await response.json();
-		console.log(flashcardsData.data);
-	}
+		const flashcardsData =await response.json();
+		const flashcardsObjArray = flashcardsData.data;
+		console.log(flashcardsObjArray);
+		
+		const prevBtn = document.getElementById("prev");
+		const nextBtn = document.getElementById("next");
+		const titleContainer = document.querySelector("#flashcard-title h2");
+		const descriptionContainer = document.querySelector("#flashcard-description h2");
+		let index = 0;
+
+		titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
+		descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
+
+		nextBtn.addEventListener("click", function (event) {
+			index++;
+			console.log(flashcardsObjArray[index].title)
+			let titleValue = flashcardsObjArray[index].title;
+			//const title = document.querySelector("#flashcard-title h2");
+			//const description = document.querySelector("#flashcard-description h2");
+
+			titleContainer.innerHTML = `${titleValue}`;
+			descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
+			return index;
+		});
+		
+
+	};
+
 
 	getAll();
 
+
+
+	
 
 
 
