ec3f626fb5ba66bb06d608807d5d7fc04a6c9adf
karolinaJot <amicus.karo@gmail.com>
2021-02-05 14:04:24 +0100

    refactor code, add comments

--- Flashcards.Web/wwwroot/js/flashcardsCarousel.js
+++ Flashcards.Web/wwwroot/js/flashcardsCarousel.js
@@ -12,78 +12,87 @@ document.addEventListener("DOMContentLoaded", function () {
 		const flashcardsObjArray = flashcardsData.data;
 
 		const btnsContainer = document.getElementById('btn-container');
+		const prevBtn = document.getElementById("prev");
+		const nextBtn = document.getElementById("next");
+		const titleContainer = document.querySelector("#flashcard-title h2");
+		const descriptionContainer = document.querySelector("#flashcard-description h2");
 
+		//Sprawdzam czy w bazie są fiszki
 		if (flashcardsObjArray.length > 0) {
 
-			const prevBtn = document.getElementById("prev");
-			const nextBtn = document.getElementById("next");
-			const titleContainer = document.querySelector("#flashcard-title h2");
-			const descriptionContainer = document.querySelector("#flashcard-description h2");
+			let index = 0;       //zmienna do nawigacji po tablicy fiszek
+
 			// wyświetlanie treści fiszki, obsługa karuzeli
-			let index = 0;
+			displayFirstFlashcard(titleContainer, descriptionContainer, flashcardsObjArray, index);
 
-			titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
-			descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
+			navigateCarousel(nextBtn, prevBtn, titleContainer, descriptionContainer, flashcardsObjArray, index);
 
-			nextBtn.addEventListener("click", event => {
-				index++;
-				if (index > flashcardsObjArray.length - 1) {
-					index = 0;
-				}
-				titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
-				descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
-				return index;
-			});
+			// wyświetlenie i obsługa buttonów CREATE, EDIT, DELETE
+			displayAllButtons(btnsContainer, flashcardsObjArray, index);
 
-			prevBtn.addEventListener("click", event => {
-				index--;
-				if (index < 0) {
-					index = flashcardsObjArray.length - 1;
-				}
-				titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
-				descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
-				return index;
-			});
-
-			// obsługa buttonów CREATE, EDIT, DELETE
 			
-			btnsContainer.innerHTML = `
-				<a class="button button--secondary flashcards-page__button" href="/Flashcards/ShowForm">
-					<i class="icon icon--xs icon--add h-margin-right-8"></i>
-					Create
-				</a>	
-				<a class="button button--secondary flashcards-page__button" href="/Flashcards/Edit/${flashcardsObjArray[index].id}">
-					<i class="icon icon--xs icon--edit h-margin-right-8"></i>
-					Edit
-				</a>
-				<a class="button button--secondary flashcards-page__button" href="/Flashcards/Delete/${flashcardsObjArray[index].id}">
-					<i class="icon icon--xs icon--delete h-margin-right-8"></i>
-					Delete
-				</a>
-				`;
 
 		} else {
 
 			alert("Sorry, there is no flashcards in here! Hit the Create button below!");
 			//jeśli w bazie nie ma fiszek to wyświetla się wyłącznie button CREATE
-			btnsContainer.innerHTML = `
+			displayCreateButton(btnsContainer);
+		}
+
+
+//---------- deklaracje funkcji użytych powyżej -------------------
+		function displayFirstFlashcard(htmlTitleEl,htmlDescrEl,data, i) {
+			htmlTitleEl.innerHTML = `${data[i].title}`;
+			htmlDescrEl.innerHTML = `${data[i].description}`;
+		}
+
+		function navigateCarousel(htmlNextBtnEl, htmlPrevBtnEl, htmlTitleEl, htmlDescrEl, data, i ) {
+			htmlNextBtnEl.addEventListener("click", event => {
+				i++;
+				if (i > data.length - 1) {
+					i = 0;
+				}
+				htmlTitleEl.innerHTML = `${data[i].title}`;
+				htmlDescrEl.innerHTML = `${data[i].description}`;
+				return i;
+			});
+
+			htmlPrevBtnEl.addEventListener("click", event => {
+				i--;
+				if (i < 0) {
+					i = data.length - 1;
+				}
+				htmlTitleEl.innerHTML = `${data[i].title}`;
+				htmlDescrEl.innerHTML = `${data[i].description}`;
+				return i;
+			});
+			return i;
+		}
+
+		function displayAllButtons(htmlDivEl ,data, i) {
+			htmlDivEl.innerHTML = `
+				<a class="button button--secondary flashcards-page__button" href="/Flashcards/ShowForm">
+					<i class="icon icon--xs icon--add h-margin-right-8"></i>
+					Create
+				</a>	
+				<a class="button button--secondary flashcards-page__button" href="/Flashcards/Edit/${flashcardsObjArray[i].id}">
+					<i class="icon icon--xs icon--edit h-margin-right-8"></i>
+					Edit
+				</a>
+				<a class="button button--secondary flashcards-page__button" href="/Flashcards/Delete/${flashcardsObjArray[i].id}">
+					<i class="icon icon--xs icon--delete h-margin-right-8"></i>
+					Delete
+				</a>
+				`;
+		}
+		function displayCreateButton(htmlDivEl) {
+			htmlDivEl.innerHTML = `
 				<a class="button button--secondary flashcards-page__button" href="/Flashcards/ShowForm">
 					<i class="icon icon--xs icon--add h-margin-right-8"></i>
 					Create
 				</a>	
 			`;
 		}
-
-
 	};
-
-
 	getAll();
-
-
-
-	
-
-
-
 });
