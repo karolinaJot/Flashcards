1973240b2f3b3ccdafad470f8a24ad5b3f00521d
Anna Mika <aniamika90@gmail.com>
2021-02-05 15:38:54 +0100

    move js code into separate file

---
+++ Flashcards.Web/wwwroot/js/flashcardSwitch.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const switchButton = document.querySelector(".flashcard-page__switch-button");
+    const flashcard = document.querySelector(".flashcard__inner");
+
+    switchButton.addEventListener('click', event => {
+        const flashcard = document.querySelector(".flashcard__inner");
+        flashcard.classList.toggle("is-flipped");
+    });
+    flashcard.addEventListener('click', event => {
+        const flashcard = document.querySelector(".flashcard__inner");
+        flashcard.classList.toggle("is-flipped");
+    });
+
+});
