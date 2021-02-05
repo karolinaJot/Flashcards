13f8ae9c8cf3e66c22abfa4ea8a18f39bc7ae2e6
karolinaJot <amicus.karo@gmail.com>
2021-02-05 12:05:54 +0100

    add "prev" navigation, add validation in JS

--- Flashcards.Web/wwwroot/js/flashcardsCarousel.js
+++ Flashcards.Web/wwwroot/js/flashcardsCarousel.js
@@ -10,35 +10,41 @@ document.addEventListener("DOMContentLoaded", function () {
 		const response = await fetch(endpoint);
 		const flashcardsData =await response.json();
 		const flashcardsObjArray = flashcardsData.data;
-		console.log(flashcardsObjArray);
-		
-		const prevBtn = document.getElementById("prev");
-		console.log(prevBtn);
-		const nextBtn = document.getElementById("next");
-		const titleContainer = document.querySelector("#flashcard-title h2");
-		const descriptionContainer = document.querySelector("#flashcard-description h2");
-		let index = 0;
 
-		titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
-		descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
+		if (flashcardsObjArray.length > 0) {
 
-		nextBtn.addEventListener("click", event => {
-			index++;
+			const prevBtn = document.getElementById("prev");
+			console.log(prevBtn);
+			const nextBtn = document.getElementById("next");
+			const titleContainer = document.querySelector("#flashcard-title h2");
+			const descriptionContainer = document.querySelector("#flashcard-description h2");
+			let index = 0;
+
 			titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
 			descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
-			return index;
-		});
 
-		console.log(index);
+			nextBtn.addEventListener("click", event => {
+				index++;
+				titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
+				descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
+				return index;
+			});
 
-		prevBtn.addEventListener("click", event => {
-			console.log("hej from prev");
-			index--;
-			titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
-			descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
-			return index;
-		});
-		
+			prevBtn.addEventListener("click", event => {
+				index--;
+				if (index > 0) {
+					titleContainer.innerHTML = `${flashcardsObjArray[index].title}`;
+					descriptionContainer.innerHTML = `${flashcardsObjArray[index].description}`;
+					return index;
+				} else {
+					alert("There is no previous flashcards. Create more flashcards or see the next one");
+				}
+			});
+
+		} else {
+
+			alert("Sorry, there is no flashcards in here! Hit the Create button below!");
+		}
 
 	};
 
