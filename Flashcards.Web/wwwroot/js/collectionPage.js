19d972bfbd5e968178c56196af7e32df00f37c08
Anna Mika <aniamika90@gmail.com>
2021-02-10 00:08:26 +0100

    Remove old icon
    Create cards view
    Implement switching between views
    Add new icons

--- Flashcards.Web/wwwroot/js/collectionPage.js
+++ Flashcards.Web/wwwroot/js/collectionPage.js
@@ -4,6 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	const endpoint = "/api/getAll";
 
+
+	function switchView() {
+		const listView = document.querySelector(".collection-page__list-view");
+		const listViewButton = document.querySelector(".collection-page__list-view-button");
+		const cardsView = document.querySelector(".collection-page__cards-view");
+		const cardsViewButton = document.querySelector(".collection-page__cards-view-button");
+		console.log(listViewButton);
+		console.log(cardsViewButton);
+
+		listViewButton.addEventListener("click", function () {
+			this.classList.toggle("button--solid");
+			cardsViewButton.classList.toggle("button--solid");
+			listView.classList.toggle("h-hidden");
+			cardsView.classList.toggle("h-show-flex");
+		});
+
+		cardsViewButton.addEventListener("click", function () {
+			this.classList.toggle("button--solid");
+			listViewButton.classList.toggle("button--solid");
+			cardsView.classList.toggle("h-show-flex");
+			listView.classList.toggle("h-hidden");
+		});
+
+	}
+	switchView();
+
+
+
 	async function getAll() {
 
 		//dodać walidację jeśli nie będzie żadnej fiszki
@@ -13,30 +41,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
 		let flashcardsList = document.getElementById("flashcardsList");
 		let createContainer = document.getElementById("createContainer");
-		let table = document.querySelector("collection-page__table");
+		const cardsView = document.querySelector(".collection-page__cards-view");
+
 
 		if (flashcardsObjArray.length > 0) {
 
 			for (let i = 0; i < flashcardsObjArray.length; i++) {
 
+				// create list view
 				let row = document.createElement("tr");
 				row.classList.add("collection-page__row");
-
 				row.innerHTML = `
 				<td class="collection-page__cell collection-page__cell--title">${flashcardsObjArray[i].title}</td>
 				<td class="collection-page__cell">
-					<a class="button button--secondary button--solid button--small collection-page__button" href="/Flashcards/Edit/${flashcardsObjArray[i].id}">
+					<a class="button button--secondary button--small collection-page__button" href="/Flashcards/Edit/${flashcardsObjArray[i].id}">
 						<i class="icon icon--xs icon--edit"></i>
 					</a>
 				</td>
 				<td class="collection-page__cell">
-					<a class="button button--secondary button--solid button--small collection-page__button" href="/Flashcards/Delete/${flashcardsObjArray[i].id}">
+					<a class="button button--secondary button--small collection-page__button" href="/Flashcards/Delete/${flashcardsObjArray[i].id}">
 						<i class="icon icon--xs icon--delete"></i>
 					</a>
 				</td>
 				`;
 				flashcardsList.appendChild(row);
 
+				// create cards view
+				const singleCard = document.createElement("div");
+				singleCard.classList.add("card");
+				cardsView.appendChild(singleCard);
+				const singleCardTitle = document.createElement("p");
+				singleCardTitle.classList.add("card__title");
+				singleCard.appendChild(singleCardTitle);
+				singleCardTitle.innerHTML += flashcardsObjArray[i].title;
 			}
 		} else {
 
@@ -54,7 +91,9 @@ document.addEventListener("DOMContentLoaded", function () {
 			`;
 		}
 
-		
 	};
 	getAll();
+
+
+
 });
