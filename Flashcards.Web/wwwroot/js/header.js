fde5ceeede3ab1afb3701cd1a0b973df94e95f36
Anna Mika <aniamika90@gmail.com>
2021-02-11 22:47:34 +0100

    Add hamburger menu on mobile

---
+++ Flashcards.Web/wwwroot/js/header.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("dziala");
+    const hamburgerButton = document.querySelector(".header__hamburger-button");
+
+    hamburgerButton.addEventListener('click', event => {
+        const headerNavigation = document.querySelector(".header__navigation");
+        headerNavigation.classList.toggle("is-open");
+        console.log("click");
+    });
+
+
+
+
+});
