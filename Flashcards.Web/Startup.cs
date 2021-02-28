fb1835ffc051aee87b277bb202cc9198014e78f0
karolinaJot <amicus.karo@gmail.com>
2021-02-28 22:35:58 +0100

    update services

--- Flashcards.Web/Startup.cs
+++ Flashcards.Web/Startup.cs
@@ -1,6 +1,7 @@
 using Flashcards.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,7 @@ namespace Flashcards.Web
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddIdentity<IdentityUser, IdentityRole>();
 			services.AddControllersWithViews();
 		}
 
