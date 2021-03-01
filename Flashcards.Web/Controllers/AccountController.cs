827f5dfdd255d7203eca3d9047fc1e23f7f41767
karolinaJot <amicus.karo@gmail.com>
2021-03-01 12:07:04 +0100

    Add registration operations

--- Flashcards.Web/Controllers/AccountController.cs
+++ Flashcards.Web/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Flashcards.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,45 @@ namespace Flashcards.Web.Controllers
 {
 	public class AccountController : Controller
 	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly SignInManager<IdentityUser> _singnInManager;
+
+		public AccountController(UserManager<IdentityUser> userManager,
+								 SignInManager<IdentityUser> signInManager)
+		{
+			_userManager = userManager;
+			_singnInManager = signInManager;
+		}
+		
+		
 		[HttpGet]
 		public IActionResult Register()
 		{
 			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+				var result = await _userManager.CreateAsync(user, model.Password);
+
+				if (result.Succeeded)
+				{
+					await _singnInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("CollectionPage", "Flashcards");
+				}
+
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError("", error.Description);
+				}
+			}
+
+
+			return View(model);
 		}
 	}
 }
